3e23634a72bea26948373e6c9e27df9272598fe2
adam-zaidi <57157067+sfdcoderaz-git@users.noreply.github.com>
2022-12-23 19:24:21 +0000

    add console log

--- slash/play.js
+++ slash/play.js
@@ -81,6 +81,7 @@ module.exports = {
                 .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
                 .setThumbnail(song.thumbnail)
                 .setFooter({ text: `Duration: ${song.duration}`})
+            console.log(url);
 		}
         if (!queue.playing) await queue.play()
         await interaction.editReply({
